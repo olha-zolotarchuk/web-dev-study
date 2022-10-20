3a3ec46900862ae7fa550e3e845eb11f99367030
olha-zolotarchuk <113042355+olha-zolotarchuk@users.noreply.github.com>
2022-10-20 22:15:56 +0200

    lesson (#28)

--- exercise/02.JS_HTML_CSS_Bootcamp/lesson-1.js
+++ exercise/02.JS_HTML_CSS_Bootcamp/lesson-1.js
@@ -1,16 +1,148 @@
-const htmlBody = document.querySelector('body');
+
+const yourFirstVariable = 'Learning to code gives you superpower';
+
+const yourSecondVariable = 10;
+
+const yourThirdVariable = { firstProperty: 'hello world' };
+
+console.log(yourFirstVariable);
+
+const newVariable = 'hello world';
 
 
+/**/
 
-const randomClickFunction = function () {
-    const colors = ["#002942", "#0CA7DB", "red", "green", "blue", "yellow", "purple"];
+// const newVariable = 'hello world';
 
-    const randomIndex = Math.floor(Math.random() * colors.length);
+let anotherVariable;
 
-    const randomColor = colors[randomIndex];
+anotherVariable = 20;
+20
 
-    htmlBody.style.background = randomColor;
-    console.log('The user clicked and set the color to' + randomColor);
+console.log(anotherVariable);
+
+/**/
+
+const variable = 10;
+
+let variable2 = 10;
+
+var variable3 = 10;
+
+/**/
+
+let counter = 0;
+
+counter = counter + 1;
+1
+
+counter++;
+1
+
+counter;
+2
+
+/**/
+
+const yourFirstVariable1 = 10
+const firstVariable = 'JavaScript gives you superpowwer'
+
+/**/
+
+const variable1 = 10;
+const variable21 = "some value";
+const variable3 = false;
+undefined
+typeof variable1;
+'number'
+typeof variable21;
+'string'
+typeof variable3;
+'boolean'
+variable1 + variable2;
+'10some value'
+const number1 = '10';
+undefined
+const number2 = 20;
+undefined
+typeof number1;
+'string'
+typeof number2;
+'number'
+number1 + number2;
+'1020'
+Number(number1) + number2;
+30
+
+/**/
+
+const stringValue = 'hello world';
+
+const anotherStringValue = "hello world";
+
+const numberValue = 20;
+
+// const numberValue4 = 20,984;
+// VM3913:1 Uncaught SyntaxError: Unexpected number
+const numberValue4 = 20.984;
+
+typeof numberValue4
+'number'
+const booleanValue = false;
+
+
+/**/
+
+const firstArray = [10, 20, 30, 40];
+
+const secondArray = [10, 'a string', { prop: 'slkdfsl' }, [1, 2]];
+
+console.log(firstArray[0]);
+10
+
+console.log(secondArray[3]);
+(2)[1, 2]
+// 0: 11: 2length: 2[[Prototype]]: Array(0)
+
+console.log([3][0]);
+3
+
+console.log(secondArray[3][0]);
+1
+
+/**/
+
+const objectVariable = { prop1: 20, prop2: 50};
+
+// objectVariable
+// {prop1: 20, prop2: 50}
+
+objectVariable[0]
+
+objectVariable.prop1
+20
+const nestedObject = {
+    layer1: {
+         layer2: {
+              layer3: {
+                  targetValue:20
+              }
+         }
+    }
 }
 
-htmlBody.onclick = randomClickFunction
+nestedObject.layer1.layer2.layer3.targetValue;
+20
+objectVariable['prop1'];
+20
+
+/**/
+
+const functionContainerVariable = function() {
+    return 20;
+}
+
+functionContainerVariable();
+20
+
+/**/
