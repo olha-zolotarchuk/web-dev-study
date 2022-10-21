4851a8f7ff4863a23b59256932c3414f5ff18fd3
Olya Zolotarchuk <zlotka1@gmail.com>
2022-10-21 12:51:53 +0200

    challenge

---
+++ exercise/02.JS_HTML_CSS_Bootcamp/challeng-1.js
@@ -0,0 +1,85 @@
+const myBoollean = true;
+
+const myString = 'hello world';
+
+const firstNumber = 20;
+
+let secondNumber = 40;
+
+secondNumber = 80;
+80
+
+const myArray = ['myBoolean', 'myString'];
+
+const myObject = {
+    firstProperty: myArray,
+    sumProperty: firstNumber + secondNumber
+};
+
+console.log(myObject);
+// { firstProperty: Array(2), sumProperty: 100 }
+
+console.log(myObject.sumProperty);
+
+console.log(myObject.firstProperty);
+// (2) ['myBoolean', 'myString']
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+
+const someNumber = 20;
+
+someNumbere = 50
+50
+
+console.log(someNumbere);
+50
+
+console.log(someNumber);
+20
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+const variable1 = 20;
+undefined
+typeof variable1;
+'number'
+const variable2 = '40';
+undefined
+typeof variable2;
+'string'
+const resultVariable = variable1 + variable2;
+undefined
+typeof resultVariable;
+'string'
+console.log(resultVariable);
+2040
+undefined
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+const objectVariable = {
+    property1: 'i am property 1',
+    property2: 'i am property 2',
+    property3: [20, 30, 40]
+};
+undefined
+console.log(objectVariable.property3[2]);
+40
+undefined
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+const myArray1 = [20, 30, 40];
+
+console.log(myArray1[3]);
+
+console.log(myArray1[2]);
+40
+
+console.log(myArray1[1]);
+30
+
+console.log(myArray1[0]);
+20
