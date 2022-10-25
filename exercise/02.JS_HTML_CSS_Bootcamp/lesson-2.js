38af1c69a0d6e2a6193ce5782a86ea09973e40b8
Olya Zolotarchuk <zlotka1@gmail.com>
2022-10-25 21:27:21 +0200

    lesson-2.3

--- exercise/02.JS_HTML_CSS_Bootcamp/lesson-2.js
+++ exercise/02.JS_HTML_CSS_Bootcamp/lesson-2.js
@@ -206,3 +206,125 @@ true
 
 /**/
 
+const numberValue2 = 20;
+undefined
+const stringValue2 = '20';
+undefined
+numberValue   !== stringValue;
+true
+numberValue   != stringValue;
+false
+'hello world' === 'hello world'
+true
+true === true;
+true
+const myObj2 = {
+    property1: 'some value',
+    property2: 20
+};
+undefined
+const myArray2 = [1, 2, 3, 4, 5];
+undefined
+myArray2 === myArray2;
+true
+const secondArray2 = [1, 2, 3, 4, 5];
+undefined
+myArray2 === secondArray2;
+false
+myObj2 === myObj2;
+true
+const anotherObj2 = {
+    property1: 'some value',
+    property2: 20
+};
+undefined
+myObj2 === anotherObj2;
+false
+myObj2 == anotherObj2;
+false
+myArray2 == secondArray2;
+false
+
+/**/
+
+const result1 = 20 === 20 ? 'values match' : 'values do not match';
+undefined
+result1;
+'values match'
+let rsultVariable;
+undefined
+if (20 === 20) {
+    resultVariable = 'values match';
+} else {
+    resultVariable = 'values do not match';
+}
+'values match'
+resultVariable
+'values match'
+const result2 = 20 === 21 ? 'values match' : 'values do not match';
+undefined
+result2
+'values do not match'
+
+/**/
+
+20 === 20 && 10 === 10;
+true
+20 === 20 && 10 === 12;
+false
+20 === 20 || 10 === 10;
+true
+false
+20 === 18 || 10 === 12;
+false
+!(20 ===20);
+false
+
+/**/
+
+const isUserLoggedIn = true;
+undefined
+const doesUserHavePermissions = false;
+undefined
+const canUserPerformAction = isUserLoggedIn && doesUserHavePermissions;
+undefined
+canUserPerformAction
+false
+
+/**/
+
+const  result3 = !(((40 / 20) === 2 && true) || ('yes' === 'no'));
+undefined
+result3
+false
+const step1 = 40 / 20;
+undefined
+const step21 = step1 === 2;
+undefined
+step21
+true
+const step3 = step21 && true;
+undefined
+step3
+true
+const step4 =  'yes' === 'no';
+undefined
+step4
+false
+const step5 = step3 || step4;
+undefined
+step5
+true
+const step6 = !step5;
+undefined
+step6
+false
+
+/**/
+
+const result5 = (() => {
+    return 20;
+})();
+undefined
+result
+20
