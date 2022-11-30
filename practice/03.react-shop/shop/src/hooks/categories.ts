37d0299e92c401b18d547ef20812ac14d4de80e0
Olya Zolotarchuk <zlotka1@gmail.com>
2022-11-30 22:16:49 +0100

    practiceShop1

---
+++ practice/03.react-shop/shop/src/hooks/categories.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useCategories = (): string[] | null=> {
+  const [categories, setCategories] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/categories/").then(async (r) => {
+      setCategories(await r.json());
+    });
+  }, []);
+  return categories;
+};
