1abd5ac2527709742f68ca52e716c53df5988637
Olya Zolotarchuk <zlotka1@gmail.com>
2022-12-06 21:49:43 +0100

    practiceShop

--- practice/03.react-shop/shop/src/components/ProductCard.tsx
+++ practice/03.react-shop/shop/src/components/ProductCard.tsx
@@ -1,20 +1,22 @@
-import { Product} from '../hooks/products'
+import { Product } from "../hooks/products";
 
 type ProductProps = {
-    product: Product | null;
-}
+  product: Product | null;
+};
 
-export const ProductCard : React.FC<ProductProps> = ({product}) => {
+export const ProductCard: React.FC<ProductProps> = ({ product }) => {
+  if (product == null) return null;
 
-    if (product == null) return null;
-
-    return <div className='product-cart'>
-        <a href={'/product/' + product.id}>    
-                <img src={product.thumbnail} alt={product.title} />
-            {product.title}
-            --- {product.price}$
-        </a>
-
+  return (
+    <div className="product-cart">
+      <a href={"/product/" + product.id}>
+        <div className="thumbnail">
+        <img src={product.thumbnail} alt={product.title} /></div> 
+        <div className="title"> {product.title}</div>
+        <div className="rating">{product.rating}</div>
+        <div className="price">{product.price}$</div>
+      </a>
     </div>
-}
+  );
+};
 // console.log(ProductCard)
