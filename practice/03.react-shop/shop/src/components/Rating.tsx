80b9c49e9656fd130793da54253e5962420fff3e
Olya Zolotarchuk <zlotka1@gmail.com>
2022-12-07 22:24:57 +0100

    practiseShop
    Former-commit-id: c8fa843f5f745e0b0564f479f038a865cb041d4e

---
+++ practice/03.react-shop/shop/src/components/Rating.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PrettyRating, { IconsInterface } from "pretty-rating-react";
+import {
+  faStar,
+  faStarHalfAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar as farStar,
+} from "@fortawesome/free-regular-svg-icons";
+
+const colors = {
+ star: ['#d9ad26', '#d9ad26', '#434b4d'],
+};
+
+
+  
+  const icons: any = {
+    star: {
+      complete: faStar,
+      half: faStarHalfAlt,
+      empty: farStar,
+    }
+  };  
+
+  type RatingProp = {
+    rating: number;
+  };
+  
+
+export const Rating: React.FC <RatingProp>= ({rating}) => (
+
+
+   <PrettyRating value={rating} max={5} icons={icons.star} colors={colors.star} />
+
+ 
+);
