51451a054a709cf985826fb1aea779101931c730
Olya Zolotarchuk <zlotka1@gmail.com>
2023-01-23 20:58:01 +0100

    js

---
+++ exercise/07. JS full course/main6.Code Challenge.js
@@ -0,0 +1,20 @@
+
+//Yor First Code Challenge
+
+// Write code that return a randomme letter from yoer name
+
+console.log("Olha")
+console.log(typeof "Olha");
+console.log("Olha".charAt());
+console.log("Olha".charAt(Math.floor(Math.random() * 4)));
+console.log("Olha".charAt(Math.floor(Math.random() * 4)));
+console.log("Olha".charAt(Math.floor(Math.random() * 4)));
+console.log("Olha".charAt(Math.floor(Math.random() * 4)));
+console.log("Olha".charAt(Math.floor(Math.random() * 4)));
+
+
+const anyName = "Anton";
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
