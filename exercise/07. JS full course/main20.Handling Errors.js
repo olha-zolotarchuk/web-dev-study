acdc665cc3d451bcdaff2cfb08c309c2aeb4ac35
Olya Zolotarchuk <zlotka1@gmail.com>
2023-01-30 12:49:01 +0100

    js
    Former-commit-id: cd0428ed534050894e7f4817b1f55fa148af3726 [formerly f50b08c792a081f92f6d92f344f2144355fefb35 [formerly f50b08c792a081f92f6d92f344f2144355fefb35 [formerly ebf2fb877bcc0bcd3685309ac1d5fad1678bc81b]]]
    Former-commit-id: e94a48b3a8228998e4b9f63b314b79fbbfa0cf09
    Former-commit-id: ce5b30456646d2aaad6086d9fe83c8b56d89f20a
    Former-commit-id: 5c59e30428c23a7e072b1484cf677e6bdbf1171f

--- exercise/07. JS full course/main20.Handling Errors.js
+++ exercise/07. JS full course/main20.Handling Errors.js
@@ -12,21 +12,21 @@
 // variable = "OOO"
 // 3) ReferenceError: Cannot access 'variable' before initializatio
 
-// const makeError = () => {
-//     try {
-//         // const name = "Olha";
-//         // name = "OOO"
-//         throw new customError ("This is a custom error")
-//     } catch (err) {
-//         // console.table(err);
-//         // console.warn(err);
-//         // console.error(err);
-//         console.error(err.name);
-//         console.error(err.message);
-//          console.error(err.stack);
-//         // logTheError(err.stack);
-//     }
-// };
+const makeError = () => {
+  try {
+    // const name = "Olha";
+    // name = "OOO"
+    throw new customError("This is a custom error");
+  } catch (err) {
+    // console.table(err);
+    // console.warn(err);
+    // console.error(err);
+    console.error(err.name);
+    console.error(err.message);
+    console.error(err.stack);
+    // logTheError(err.stack);
+  }
+};
 // makeError();
 
 // function customError(message) {
@@ -38,22 +38,65 @@
 // generic error constractor
 // Tri catch blocks can also include finally blocks. Finally blocks come at the end and they execute no matter what. So if an error is thrown, the finally block will still execute after the catch block, if no error is thrown, the finally block will still execute as well.
 
-const makeError = () => {
-  let i = 1;
-  while (i <= 5) {
-    try {
-      if (i % 2 !== 0) {
-        throw new Error("Odd number");
-      }
-        console.log("Even number")
-    } catch (err) {
-      console.error(err.stack);
-    } finally {
-      console.log("...finally");
-      i++;
-    }
-  }
-};
-makeError();
+// const makeError = () => {
+//   let i = 1;
+//   while (i <= 5) {
+//     try {
+//       if (i % 2 !== 0) {
+//         throw new Error("Odd number");
+//       }
+//       console.log("Even number");
+//     } catch (err) {
+//       console.error(err.stack);
+//     } finally {
+//       console.log("...finally");
+//       i++;
+//     }
+//   }
+// };
+// makeError();
 
 //  Finally, we'll execute no matter what, whether there's an error or not.  Catch will only execute if there is an error and if there is an error in the tri block, any code following that error will not execute. Any code that remains in the tri block will not execute if an error happens before it reaches that code. So this line 48 only executed when we had an even number and that is how try-catch and finally work.
+
+// Of course, the other error types, like a syntax error, a reference error, a type error, those are also constructors.
+
+//real time , runtime
+
+function increment(a) {
+  if (a == 7) {
+    throw new Error("Can't increament 7");
+  }
+  return a + 1;
+}
+
+function incDevide(a, b) {
+  if (b == 0) {
+    throw new Error("Impossible to devide by 0");
+  }
+  //   try {
+  return increment(a) / increment(b);
+  //   } catch (e) {
+  // return 777;
+  //   }
+}
+
+// try {
+// console.log(incDevide(8, 7));
+// } catch (e) {
+//   console.error(e);
+// }
+console.log("test");
+
+//
+
+function rating(value) {
+    if (value > 10) {
+      
+    throw new Error("Out of range val < 10");
+  }
+  return value;
+}
+
+//
+
+rating(9);
