55b2bbe986afb4d2a368d91a08e3320630e289eb
Olya Zolotarchuk <zlotka1@gmail.com>
2023-02-13 16:26:23 +0100

    react
    Former-commit-id: d60eb032597977238cf144d24928f979f9ae6217 [formerly a980e25ac2f8606004290432ce4bfcce43d934d5 [formerly a980e25ac2f8606004290432ce4bfcce43d934d5 [formerly 665871268ee4cb6722f90a596996b5c3bb68e844]]]
    Former-commit-id: 652c554a497e88c71517a2bcb661a457f82fa61a
    Former-commit-id: 492eed4d3cc9504846a6852ebce89309ea5f0b51
    Former-commit-id: 7ad95d80d97014fdd3ad163277751e82bdc6ae3b

--- exercise/08.React_JS/01.react/src/App.js
+++ exercise/08.React_JS/01.react/src/App.js
@@ -5,21 +5,40 @@ import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
+import apiRequest from "./apiReguest";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist")) || []
-  );
+  const API_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([]);
+
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   // const [a, setA] = useState("");
   // const [c, setC] = useState("");
   // const [b, setB] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  }, [items]);
+    // localStorage.setItem("shoppinglist", JSON.stringify(items));
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive expected data");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    setTimeout(() => fetchItems(), 2000);
+  }, []);
 
   // useEffect(() => {
   //   console.log("rerender");
@@ -31,25 +50,52 @@ function App() {
   //   console.log(a, b, c);
   // }, [c]);
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myNewItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     // console.log(`key: ${id}`);
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     // console.log(id);
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = { method: "DELETE" };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
   const handleSubmit = (e) => {
@@ -71,17 +117,24 @@ function App() {
       <SearchItem search={search} setSearch={setSearch} />
       {/* <Header /> */}
       {/* <Content /> */}
-      <Content
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
+
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      </main>
+
       <Footer length={items.length} />
 
-
-{/* 
+      {/* 
       <button
         onClick={() => {
           setA("1");
@@ -104,8 +157,6 @@ function App() {
         C
       </button> */}
 
-
-      
       {/* <p className="App-intro">Hello {handleNameChange()}!</p> */}
       {/* <p> {name}</p> */}
     </div>
