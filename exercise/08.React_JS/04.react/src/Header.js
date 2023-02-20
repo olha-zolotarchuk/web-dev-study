b5c3a0515f05503629db1788df65a1443a0933c1
Olya Zolotarchuk <zlotka1@gmail.com>
2023-02-20 16:12:42 +0100

    react
    Former-commit-id: c4cc2fabe9b04926e7413d1022821462a869740b [formerly 4cd59d5e93acf27090757becc435436bd6e28e88 [formerly 4cd59d5e93acf27090757becc435436bd6e28e88 [formerly 0ca7a26eba78704f4f4cd8fc861763e77fbf4f91]]]
    Former-commit-id: 19383cab341813f6eb738a362fa1e914c73f5485
    Former-commit-id: cbe7edad3270b69353a5f47e6b162741a6e6f4b9
    Former-commit-id: f351875d837a7001107e5e109fe1865eab78a807

--- exercise/08.React_JS/04.react/src/Header.js
+++ exercise/08.React_JS/04.react/src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+import useWindowSize from "./hooks/useWindowSize";
 
-const Header = ({ title, width }) => {
+const Header = ({ title }) => {
+
+  const { width } = useWindowSize();
+  
   return (
     <React.Fragment>
       <header className="Header">
--- exercise/08.React_JS/04.react/src/Home.js
+++ exercise/08.React_JS/04.react/src/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Feed from "./Feed";
+import { useContext } from "react";
+import DataContext from "./context/DataContext";
 
-const Home = ({ posts, fetchError, isLoading }) => {
+const Home = () => {
+  const { searchResults, fetchError, isLoading } = useContext(DataContext);
+
   return (
     <main className="Home">
       {/* {        // posts !== undefined &&
@@ -20,8 +24,8 @@ const Home = ({ posts, fetchError, isLoading }) => {
       )}
       {!isLoading &&
         !fetchError &&
-        (posts.length ? (
-          <Feed posts={posts} />
+        (searchResults.length ? (
+          <Feed posts={searchResults} />
         ) : (
           <p className="statusMsg">No posts to display.</p>
         ))}
