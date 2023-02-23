faae6fe0d1b3c1b1181c1b08f2248d711c74b490
Olya Zolotarchuk <zlotka1@gmail.com>
2023-02-23 10:36:13 +0100

    react-practice
    Former-commit-id: cbcb6b781072d417f35cd0e73f0bf60bd54bff03 [formerly ae2c3931bda087dcfea416b738fab98b659ac621 [formerly ae2c3931bda087dcfea416b738fab98b659ac621 [formerly aec8963a8ddd2313e585be1426a0a6811ad2bec7]]]
    Former-commit-id: f2dbf008b1705a72d6eabd158ba878c39f29dc10
    Former-commit-id: 0261925855ba8764e3ffeb146d82e3313c5bf53f
    Former-commit-id: def9fe2a998bd3a41a36e753deb2f62222fcf470

--- practice/05.react_shopping_cart/src/App.js
+++ practice/05.react_shopping_cart/src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import Cart from "./Cart";
+// import Cart from "./Cart";
+import ProductCart from "./Product/ProductsCart";
 
 function App() {
-   const API_URL = "https://jsonplaceholder.typicode.com/";
+  // const API_URL = "https://dummyjson.com/products";
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        // console.log(data)
+        setProducts(data.products);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <React.Fragment>
       <div className="App">
         <Nav />
-        <Cart />
+        {/* <Cart products={products} /> */}
+        <ProductCart products={products} />
         <Footer />
       </div>
     </React.Fragment>
