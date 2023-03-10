1de6d7d58904ae47c266f75c8b3f4a4215f2a91c
Olya Zolotarchuk <zlotka1@gmail.com>
2023-03-10 09:36:00 +0100

    react-practice

---
+++ practice/05.react_shopping_cart/src/UseTotalCart.js
@@ -0,0 +1,42 @@
+import { useCart } from "./services/useCart";
+import { useProducts } from "./services/useProducts";
+import { useState, useEffect } from "react";
+
+const UseTotalCart = () => {
+  const products = useProducts();
+  const cart = useCart();
+
+  const [totalPrice, setTotalPrice] = useState(0);
+  // console.log(products);
+
+  useEffect(() => {
+    if (
+      products == undefined ||
+      products.length == 0 ||
+      cart == undefined ||
+      cart.length == 0
+    ) {
+      return;
+    }
+
+    // var total = 0;
+    // cart.forEach(
+    //   (cartItem) =>
+    //     (total +=
+    //       products.find((p) => p.id === cartItem.id).price * cartItem.amount)
+    // );
+
+    setTotalPrice(
+      cart.reduce(
+        (total, cartItem) =>
+          products.find((p) => p.id === cartItem.id).price * cartItem.amount +
+          total,
+        0
+      )
+    );
+  }, [products, cart]);
+
+  return <div className="UseTotalCart">{totalPrice}</div>;
+};
+
+export default UseTotalCart;
--- practice/05.react_shopping_cart/src/services/useCart.js
+++ practice/05.react_shopping_cart/src/services/useCart.js
@@ -5,21 +5,25 @@ export const useCart = () => {
   const API_URL_CART = "http://localhost:3600/cart";
   const [cart, setCart] = useState([]);
 
+  useEffect(
+    () => {
+      const fetchCart = async () => {
+        try {
+          const response = await fetch(API_URL_CART);
+          const dataCart = await response.json();
+          // console.log(dataCart);
 
-  useEffect(() => {
-    const fetchCart = async () => {
-      try {
-        const response = await fetch(API_URL_CART);
-        const dataCart = await response.json();
-        // console.log(dataCart);
-        
-        setCart(dataCart);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+          setCart(dataCart);
+        } catch (err) {
+          console.log(err);
+        }
+      };
 
-    fetchCart();
-  }, []);
+      fetchCart();
+    },
+    []
+  );
+
+  
   return cart;
 };
