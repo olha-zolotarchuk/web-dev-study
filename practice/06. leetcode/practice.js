8939e5301facf9a4344e75ee3d7f3846c900d5ff
Olya Zolotarchuk <zlotka1@gmail.com>
2023-03-17 16:45:15 +0100

    practice

--- practice/06. leetcode/practice.js
+++ practice/06. leetcode/practice.js
@@ -12,23 +12,34 @@
 
 // https://leetcode.com/problems/roman-to-integer/
 
-function romanToInt(s) {
-  const num = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
+// function romanToInt(s) {
+//   const num = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000,
+//   };
 
-  for (let i = 0; i < s.length; i++) {
-    console.log(s[i]);
-    console.log(num[s[i]]);
-  }
-}
-romanToInt("VIII");
+//   let result = 0;
+//   let test = 0;
+
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     const number = num[s[i]];
+
+//     if (number < test) {
+//       result -= number;
+//     } else {
+//       result += number;
+//     }
+//    test = number;
+//   }
+//   return result;
+// }
+// romanToInt("");
+// console.log(romanToInt("VI"));
 
 // const symbol = ["I", "V", "X", " L", "C", "D", "M"];
 // const value = [1,5,10,50,100,500,1000]
@@ -39,4 +50,40 @@ romanToInt("VIII");
 // console.log(val);
 // num.find((element) => console.log(element.symbol));
 
-// https://leetcode.com/problems/longest-common-prefix/description/
+// https://leetcode.com/problems/valid-parentheses
+
+
+
+  // const bracket= {
+  //   "(": ")",
+  //   "{": "}",
+  //   "[": "]",
+  // };
+
+
+
+
+  for (let i = 0; i < 9; i++) {
+    console.log(i);
+    // more statements
+  }
+
+  function plusOne(digits) {
+    // Loop through the digits array starting from the end
+    for (let i = digits.length - 1; i >= 0; i--) {
+      // Increment the current digit by one
+      digits[i]++;
+
+      // If the current digit is less than 10, we don't need to carry over to the next digit
+      if (digits[i] < 10) {
+        return digits;
+      }
+      // Otherwise, we need to carry over to the next digit and set the current digit to 0
+      digits[i] = 0;
+    }
+
+    // If we have looped through the entire digits array and haven't returned, we need to add an extra digit to the beginning of the array
+    digits.unshift(1);
+
+    return digits;
+  }
