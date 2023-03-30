78b0fb8b902d5c9be281ae4bce2ddcef488eb73b
Olya Zolotarchuk <zlotka1@gmail.com>
2023-03-30 10:34:30 +0200

    practice-css

--- exercise/03.Adaptive site layout HTML,CSS/script.js
+++ exercise/03.Adaptive site layout HTML,CSS/script.js
@@ -1,9 +1,9 @@
-const swiper = new Swiper('.slider-main-block', {
-    // Optional parameters
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-main-block__arrow.swiper-button-next',
-      prevEl: '.lider-main-block__arrow.swiper-button-prev',
-    },
-  });
+const swiper = new Swiper(".slider-main-block", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".body-main-block__arrow.swiper-button-next",
+    prevEl: ".body-main-block__arrow.swiper-button-prev",
+  },
+});
